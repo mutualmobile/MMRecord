e9e7da7db2f24db8ff5d5b1447dafe3181ea770b
Conrad Stoll <conrad.stoll@mutualmobile.com>
2013-06-14 22:39:46 -0700

    Removing a log

--- Source/MMRecord/MMRecordResponse.m
+++ Source/MMRecord/MMRecordResponse.m
@@ -330,7 +330,6 @@
 #pragma mark - Accessors
 
 - (MMRecordProtoRecord *)protoRecordForPrimaryKeyValue:(id)primaryKeyValue {
-    NSLog(@"%d", [[self protoRecords] count]);
     for (MMRecordProtoRecord *proto in self.protoRecords) {
         if ([proto.primaryKeyValue isEqual:primaryKeyValue]) {
             return proto;
