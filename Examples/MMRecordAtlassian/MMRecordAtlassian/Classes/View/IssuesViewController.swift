67139cb148fa211ca29e6853e2ba1bd5b4431e2a
Conrad Stoll <cnstoll@me.com>
2014-06-24 20:20:15 -0500

    Updates to Atlassian example UI

---
+++ Examples/MMRecordAtlassian/MMRecordAtlassian/Classes/View/IssuesViewController.swift
@@ -0,0 +1,15 @@
+//
+//  IssuesViewController.swift
+//  MMRecordAtlassian
+//
+//  Created by Conrad Stoll on 6/24/14.
+//  Copyright (c) 2014 Mutual Mobile. All rights reserved.
+//
+
+import UIKit
+
+class IssuesViewController: UITableViewController {
+
+       
+
+}
